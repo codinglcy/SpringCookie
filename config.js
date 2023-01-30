75d2f389c6bd1e9fcb43a3e02ab7344ddc7f1e0b
codinglcy <codinglcy@gmail.com>
2023-01-30 22:40:09 +0900

    :tada: 프로젝트 시작

---
+++ config.js
@@ -0,0 +1,18 @@
+// import 'dotenv/config';
+
+// const config = {
+// 	development: {
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: process.env.MYSQL_ROOT_PASSWORD,
+// 		database: 'springcookie',
+// 	},
+// 	test: {
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: process.env.MYSQL_ROOT_PASSWORD,
+// 		database: 'springcookie_test',
+// 	},
+// };
+
+// export { config };
---
+++ src/db/db-connect.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import mysql from 'mysql2';
+// import { config } from '../../config';
+
+const dbcon = mysql.createConnection({
+	host: 'localhost',
+	port: '3306',
+	user: 'root',
+	password: process.env.MYSQL_ROOT_PASSWORD,
+	database: 'springcookie',
+});
+
+export { dbcon };
