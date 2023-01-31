448b7d176d266feb7f71e365a92996e19ec7617d
codinglcy <codinglcy@gmail.com>
2023-01-31 19:03:20 +0900

    🔥🎨 서버 연결, 불필요한 코드 삭제
    - db연결 확인 코드 삭제
    - 서버 연결이 안되는 문제 해결 : 실수로 port번호 입력 누락

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import 'dotenv/config';
 import { app } from './src/app.js';
-import { dbcon } from './src/db/db-connect.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(() => {
+app.listen(PORT, () => {
 	console.log(`정상적으로 서버를 시작하였습니다. http://localhost:${PORT}`);
-});
-
-dbcon.connect((err) => {
-	if (err) {
-		console.log(`db 연결에 실패하였습니다. ${err}`);
-	} else {
-		console.log(`db 연결에 성공하였습니다.`);
-	}
 });
