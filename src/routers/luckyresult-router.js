c0b2c30c46dd44e024b9bc41164db39cc29de7bf
codinglcy <codinglcy@gmail.com>
2023-01-31 21:06:36 +0900

    🎨 참조테이블 수정, 변수명 수정
    - word테이블 참조 -> result테이블 참조

--- src/routers/luckyresult-router.js
+++ src/routers/luckyresult-router.js
@@ -3,15 +3,19 @@ import { dbcon } from '../db-connect.js';
 
 const luckyResultRouter = Router();
 
-luckyResultRouter.get('/random', async (req, res, next) => {
+luckyResultRouter.get('/', async (req, res, next) => {
 	try {
-		dbcon.query('select count(*) as count from word', (err, wordCount) => {
-			const randomNum = 1 + Math.floor(Math.random() * wordCount[0].count);
-			const findRandomQuery = `select * from word where id=${randomNum}`;
-			dbcon.query(findRandomQuery, (err, randomResult) => {
-				res.status(201).json(randomResult);
-			});
-		});
+		dbcon.query(
+			'select count(*) as count from result',
+			(err, sentenceCount) => {
+				const randomNum =
+					1 + Math.floor(Math.random() * sentenceCount[0].count);
+				const findRandomQuery = `select * from result where id=${randomNum}`;
+				dbcon.query(findRandomQuery, (err, randomResult) => {
+					res.status(201).json(randomResult);
+				});
+			},
+		);
 	} catch (err) {
 		next(err);
 	}
